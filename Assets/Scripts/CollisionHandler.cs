9a24fbe6eea66afca208398140cb242256faccd9
Evweb57 <ericvanmeter57@gmail.com>
2021-06-18 11:32:12 -0500

    Created next level and methods
    Created methods for progressing and failing the game task.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float delay = 1f;
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -11,12 +12,23 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("This thing is friendly.");
                 break;
             case "Finish":
-                Debug.Log("This is the finish!");
+                StartNextLevelLoad();
                 break;
             default:
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", delay);
+    }
+    void StartNextLevelLoad()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("NextLevel", delay);
     }
 
     void ReloadLevel()
@@ -25,4 +37,14 @@ public class CollisionHandler : MonoBehaviour
         SceneManager.LoadScene(currentSceneIndex);
     }
 
+    void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
 }
