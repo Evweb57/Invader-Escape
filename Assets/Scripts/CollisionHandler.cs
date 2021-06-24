a9fb9254ef02d63994871d080483a959629a94a0
Evweb57 <ericvanmeter57@gmail.com>
2021-06-24 12:29:23 -0500

    Created debug cmds
    Added collision toggle and next level cycle.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -13,15 +13,33 @@ public class CollisionHandler : MonoBehaviour
     AudioSource audioSource;
 
     bool isTransitioning = false;
+    bool collisionDisabled = false;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
     }
 
+    void Update()
+    {
+        CheatCommands();
+    }
+
+    void CheatCommands()
+    {
+        if (Input.GetKey(KeyCode.L))
+        {
+            NextLevel();
+        }
+        if (Input.GetKey(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled;
+        }
+    }
+
     void OnCollisionEnter(Collision other)
     {
-        if (isTransitioning) { return; }
+        if (isTransitioning || collisionDisabled) { return; }
         switch (other.gameObject.tag)
         {
             case "Friendly":
@@ -45,6 +63,7 @@ public class CollisionHandler : MonoBehaviour
         GetComponent<Movement>().enabled = false;
         Invoke("ReloadLevel", delay);
     }
+
     void StartNextLevelLoad()
     {
         isTransitioning = true;
